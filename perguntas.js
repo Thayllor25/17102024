17d94e1e7f6351490920b3e89128e6a6502aee6a
Thayllor25 <137410498+Thayllor25@users.noreply.github.com>
2024-10-24 16:37:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'Uma variável é um espaço na memória que armazena dados. Você pode dar um nome a esse espaço e usar para referenciar os dados armazenados.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    'Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'O que faz a função print() em Python?',
+    'A função print() exibe o valor passado como argumento na saída padrão (geralmente o console).'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Boa tarde em Inglês?',
+    'Good afternoon'
+)
